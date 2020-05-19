92bdae66bc6ac9b6ef8395d7743f476e6f30986d
Yara Alkassar <yara.sarmad@yahoo.com>
2020-05-19 23:06:16 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-
-
-  let btn = document.getElementById('btn'); //get button
-  btn.addEventListener('click', function(e)
-  {      e.preventDefault();
-      let list = document.getElementById('tasks');
-      let listItem = document.createElement('li');
-      const taskName = document.getElementById('new-task-description').value;
-      listItem.innerHTML = taskName;
-      list.appendChild(listItem);
+  document.getElementById("submit-button").addEventListener('click', function(e){
+    e.preventDefault();
+    let list = document.getElementById("tasks");
+    let listItem = document.createElement("li");
+    const taskName = document.getElementById("new-task-description");
+    listItem.innerHTML = taskName.value;
+    list.appendChild(listItem);
   });
 });
