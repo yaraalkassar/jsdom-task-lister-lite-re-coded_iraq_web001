1a76fee20a41496246f2515e926d5078e47e61c1
Yara Alkassar <yara.sarmad@yahoo.com>
2020-05-19 23:05:15 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ document.addEventListener("DOMContentLoaded", () => {
   let btn = document.getElementById('btn'); //get button
   btn.addEventListener('click', function(e)
   {      e.preventDefault();
-      let taskName = document.getElementById('new-task-description').value;
       let list = document.getElementById('tasks');
       let listItem = document.createElement('li');
+      const taskName = document.getElementById('new-task-description').value;
       listItem.innerHTML = taskName;
       list.appendChild(listItem);
   });
