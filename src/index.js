9095b583fb322fac4fdf087f2229152a1dca48ad
Yara Alkassar <yara.sarmad@yahoo.com>
2020-05-19 22:47:32 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let btn = document.getElementById('btn'); //get button
+  btn.addEventListener('click', addToList);
+  function addToList() {
+      hiddenElemetns= document.getElementsByClassName('hide');
+      for(let i=0; i<hiddenElemetns.length; i++)
+    {
+        hiddenElemetns[i].style.display = "block"; 
+    }
+      let taskName = document.getElementById('task').value;
+      let list = document.getElementById('list');
+      let listItem = document.createElement('li');
+      listItem.innerHTML = listText;
+      list.appendChild(listItem);
+      let form = document.getElementById('taskForm');
+      form.reset();
+  }
+
+
+
+
+
+
+
+
+
+
+
+  
 });
