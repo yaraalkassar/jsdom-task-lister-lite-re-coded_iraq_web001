7c8ddf6bc622fc06087d6cf4d36a590dc9386ab8
Yara Alkassar <yara.sarmad@yahoo.com>
2020-05-19 23:10:48 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("btn").addEventListener('click', function(e){
+    e.preventDefault();
+    let list = document.getElementById("tasks");
+    let listItem = document.createElement("li");
+    const taskName = document.getElementById("new-task-description").value;
+    listItem.innerHTML = taskName;
+    list.appendChild(listItem);
+  });
 });
